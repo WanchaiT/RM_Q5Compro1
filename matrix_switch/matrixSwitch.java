bc80778a7f66737c2137a3fbb7d6999d3e9195ed
Wanchai Thiantanawat <wanchai.thiantanawat@hotmail.com>
2019-01-26 15:19:42 +0700

    Matrix

---
+++ matrix_switch/matrixSwitch.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class matrixSwitch {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[][] data = new int[n][n];
+
+        for (int i = 0;i < n ;i++ ) {
+            for (int j = 0;j < n ;j++ ) {
+                data[i][j] = scan.nextInt();
+            }
+        }
+
+        int command = scan.nextInt();
+        int tempData ;
+
+        if (command == 1) {
+            for (int i = 0;i < n ;i++ ) {
+                for (int j = 0;j < n/2 ;j++ ) {
+                    tempData = data[i][j];
+                    data[i][j] = data[i][j+n/2];
+                    data[i][j+n/2] = tempData;
+                }
+            }
+        }else{
+            for (int i = 0;i < n/2 ;i++ ) {
+                for (int j = 0;j < n ;j++ ) {
+                    tempData = data[i][j];
+                    data[i][j] = data[i+n/2][j];
+                    data[i+n/2][j] = tempData;
+                }
+            }
+        }
+
+        for (int i = 0;i < n ;i++ ) {
+            for (int j = 0;j < n ;j++ ) {
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
